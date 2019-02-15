d223499d80d3902bd48e6983397e59dbbd87dec2
David Nguyen <david.duy.minh@gmail.com>
2019-02-15 19:59:33 +0000

    added rock_paper_scissors game

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -11,23 +11,20 @@ end
 def comma_my_number(number)
   num_arr = number.split('')
   i = (num_arr.length - 3) / 3
-  # we minus 6 to get to a point 3 digits from the end of the number, taking the 2 decimal numbers into account
+  # we minus 6 to get to a point 3 digits from the end of the number, taking the
+  # 2 decimal numbers into account
   curr_comma = num_arr.length - 6
-  
-  binding.pry
-  
+
   while i > 0
     num_arr.insert(curr_comma, ',')
     curr_comma -= 3
     i -= 1
-    
-    binding.pry
   end
-  
-  return num_arr.join
+
+  num_arr.join
 end
 
-def get_value()
+def request_value
   value_here = ''
   loop do
     value_here = gets.chomp
@@ -37,42 +34,33 @@ def get_value()
       prompt("invalid entry, please enter a valid value")
     end
   end
-  
-  return value_here.to_f
+
+  value_here.to_f
 end
 
-def main()
-  # p = principle amount
-  p = ''
-  apr = ''
-  duration_years = ''
-  
-  prompt("welcome to our mortgage calculator!")
-  
-  prompt("how much is your total loan amount?")
-  p = get_value()
-  
-  prompt("what is your Annual Percentage Rate?")
-  apr = get_value()
-  apr /= 100
-  
-  prompt("how many years is left on your mortgage?")
-  duration_years = get_value()
-  
-  # j = monthly interest rate
-  j = apr / 12
-  
-  # n = number of months on mortgage
-  n = duration_years * 12
-  
-  # m = monthly payment
-  m = p * (j / (1 - (1 + j)**(-n)))
-  
-  if m.round(2).to_s.length > 5
-    m = comma_my_number(m.round(2).to_s)
-  end
-  
-  prompt("Your monthly payment is #{m}")
+prompt("welcome to our mortgage calculator!")
+
+prompt("how much is your total loan amount?")
+p = request_value()
+
+prompt("what is your Annual Percentage Rate?")
+apr = request_value()
+apr /= 100
+
+prompt("how many years is left on your mortgage?")
+duration_years = request_value()
+
+# j = monthly interest rate
+j = apr / 12
+
+# n = number of months on mortgage
+n = duration_years * 12
+
+# m = monthly payment
+m = p * (j / (1 - (1 + j)**(-n)))
+
+if m.round(2).to_s.length > 5
+  m = comma_my_number(m.round(2).to_s)
 end
 
-main()
+prompt("Your monthly payment is #{m}")
---
+++ lesson_2/rock_paper_scissors_game.rb
@@ -0,0 +1,47 @@
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+def test_method
+  prompt("This is a test message")
+end
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def display_results(player, computer)
+  if (player == 'rock' && computer == 'scissors') ||
+     (player == 'scissors' && computer == 'paper') ||
+     (player == 'paper' && computer == 'rock')
+    "You won! Awesome stuff, weirdo!"
+  elsif computer == player
+    "It was a tie!"
+  else
+    "Computer won...it's okay, AI IS going to take over the world anyways"
+  end
+end
+
+prompt("Welcome to our Rock, Paper, Scissors game!")
+
+loop do
+  choice = ''
+  loop do
+    prompt("What is your choice? (#{VALID_CHOICES.join(', ')})")
+    choice = gets.chomp.downcase
+
+    break if VALID_CHOICES.include?(choice)
+    prompt("Incorrect choice, please try again...idiot")
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt("You picked: #{choice}")
+  prompt("Computer picked: #{computer_choice}")
+  prompt("----------------------------")
+  prompt(display_results(choice, computer_choice))
+  prompt("----------------------------")
+
+  prompt("Would you like to play again? ")
+  break unless gets.chomp.downcase.start_with?('y')
+end
+
+prompt("Thanks for playing! Now go enjoy ice cream")
